76420d096993b600e6aa7366c5de9b5d11b9de5f
anujshukla03 <123716196+anujshukla03@users.noreply.github.com>
2023-06-27 10:39:17 +0530

    merged code

--- apps/home/itmBreakoutAlert.py
+++ apps/home/itmBreakoutAlert.py
@@ -11,16 +11,55 @@ import math
 class breakoutLogic():
 
     def __init__(self) -> None:
-        # self.establish_db()
-        pass
 
-    
+        print('Object Created')
+
+        self.establish_db()
+        
+        print("orb_range_candle_time:", self.orb_range_candle_time, "(", type(self.orb_range_candle_time) ,")")
+        print("or_breakout_candle_time:", self.or_breakout_candle_time, "(", type(self.or_breakout_candle_time) ,")")
+        print("orb_ma_h:", self.orb_ma_h, "(", type(self.orb_ma_h) ,")")
+        print("orb_ma_l:", self.orb_ma_l, "(", type(self.orb_ma_l) ,")")
+        print("orb_range_start_time:", self.orb_range_start_time, "(", type(self.orb_range_start_time) ,")")
+        print("orb_retracement_time:", self.orb_retracement_time, "(", type(self.orb_retracement_time) ,")")
+        print("itm_ma_h:", self.itm_ma_h, "(", type(self.itm_ma_h) ,")")
+        print("itm_ma_l:", self.itm_ma_l, "(", type(self.itm_ma_l) ,")")
+        print("itm_ma_oi:", self.itm_ma_oi, "(", type(self.itm_ma_oi) ,")")
+        print("itm_reentry_after_mins:", self.itm_reentry_after_mins, "(", type(self.itm_reentry_after_mins) ,")")
+        print("itm_entry_points_difference:", self.itm_entry_points_difference, "(", type(self.itm_entry_points_difference) ,")")
+        print("itm_exit_points_difference:", self.itm_exit_points_difference, "(", type(self.itm_exit_points_difference) ,")")
+        print("itm_sl_points_difference:", self.itm_sl_points_difference, "(", type(self.itm_sl_points_difference) ,")")
+        print("itm_sl_cost_points_difference:", self.itm_sl_cost_points_difference, "(", type(self.itm_sl_cost_points_difference) ,")")
+        print("itm_vwap_points_difference:", self.itm_vwap_points_difference, "(", type(self.itm_vwap_points_difference) ,")")
+        print("itm_sold_option_premium_decay:", self.itm_sold_option_premium_decay, "(", type(self.itm_sold_option_premium_decay) ,")")
+        print("itm_profit_percent:", self.itm_profit_percent, "(", type(self.itm_profit_percent) ,")")
+        print("itm_profit_increment:", self.itm_profit_increment, "(", type(self.itm_profit_increment) ,")")
+        print("itm_first_target_qty:", self.itm_first_target_qty, "(", type(self.itm_first_target_qty) ,")")
+        print("itm_second_target_qty:", self.itm_second_target_qty, "(", type(self.itm_second_target_qty) ,")")
+        print("itm_order_type:", self.itm_order_type, "(", type(self.itm_order_type) ,")")
+        print("itm_last_entry_condition_check_time:", self.itm_last_entry_condition_check_time, "(", type(self.itm_last_entry_condition_check_time) ,")")
+        print("itm_pyramid_start_time:", self.itm_pyramid_start_time, "(", type(self.itm_pyramid_start_time) ,")")
+        print("itm_last_pyramid_condition_check_time:", self.itm_last_pyramid_condition_check_time, "(", type(self.itm_last_pyramid_condition_check_time) ,")")
+        print("itm_second_tranche_time_diffence_mins:", self.itm_second_tranche_time_diffence_mins, "(", type(self.itm_second_tranche_time_diffence_mins) ,")")
+        print("itm_order_qty:", self.itm_order_qty, "(", type(self.itm_order_qty) ,")")
+        print("itm_order_multiplier:", self.itm_order_multiplier, "(", type(self.itm_order_multiplier) ,")")
+        print("nifty_instrument_token:", self.nifty_instrument_token, "(", type(self.nifty_instrument_token) ,")")
+        print("nifty_hl_difference_points:", self.nifty_hl_difference_points, "(", type(self.nifty_hl_difference_points) ,")")
+        print("nifty_hl_difference_points:", self.nifty_hl_difference_points, "(", type(self.nifty_hl_difference_points) ,")")
+        print("nifty_or_breakout_range_point_diff:", self.nifty_or_breakout_range_point_diff, "(", type(self.nifty_or_breakout_range_point_diff) ,")")
+        print("bankNifty_instrument_token:", self.bankNifty_instrument_token, "(", type(self.bankNifty_instrument_token) ,")")
+        print("bankNifty_hl_difference_points:", self.bankNifty_hl_difference_points, "(", type(self.bankNifty_hl_difference_points) ,")")
+        print("bankNifty_or_range_point_difference:", self.bankNifty_or_range_point_difference, "(", type(self.bankNifty_or_range_point_difference) ,")")
+        print("bankNifty_or_breakout_range_point_diff:", self.bankNifty_or_breakout_range_point_diff, "(", type(self.bankNifty_or_breakout_range_point_diff) ,")")
+        print("finNifty_instrument_token:", self.finNifty_instrument_token, "(", type(self.finNifty_instrument_token) ,")")
+        print("finNifty_hl_difference_points:", self.finNifty_hl_difference_points, "(", type(self.finNifty_hl_difference_points) ,")")
+        print("finNifty_or_range_point_difference:", self.finNifty_or_range_point_difference, "(", type(self.finNifty_or_range_point_difference) ,")")
+        print("finNifty_or_breakout_range_point_diff:", self.finNifty_or_breakout_range_point_diff, "(", type(self.finNifty_or_breakout_range_point_diff) ,")")
+ 
+ 
     def establish_db(self):
 
         self.kite = kiteInit()
-        self.strategy_status = self.kite.strategy_status["strategy_status"]
-        self.updated_by = self.kite.strategy_status["updated_by"]
-        self.updated_on = self.kite.strategy_status["updated_on"]
         self.orb_range_candle_time = self.kite.generic_params["orb_range_candle_time"]
         self.or_breakout_candle_time = self.kite.generic_params["or_breakout_candle_time"]
         self.orb_ma_h = self.kite.generic_params["orb_ma_h"]
@@ -67,48 +106,11 @@ class breakoutLogic():
         self.finNifty_or_range_point_difference = self.kite.finnifty_params["finNifty_or_range_point_difference"]
         self.finNifty_or_breakout_range_point_diff = self.kite.finnifty_params["finNifty_or_breakout_range_point_diff"]
 
-
-
-        print("orb_range_candle_time:", self.orb_range_candle_time, "(", type(self.orb_range_candle_time) ,")")
-        print("or_breakout_candle_time:", self.or_breakout_candle_time, "(", type(self.or_breakout_candle_time) ,")")
-        print("orb_ma_h:", self.orb_ma_h, "(", type(self.orb_ma_h) ,")")
-        print("orb_ma_l:", self.orb_ma_l, "(", type(self.orb_ma_l) ,")")
-        print("orb_range_start_time:", self.orb_range_start_time, "(", type(self.orb_range_start_time) ,")")
-        print("orb_retracement_time:", self.orb_retracement_time, "(", type(self.orb_retracement_time) ,")")
-        print("itm_ma_h:", self.itm_ma_h, "(", type(self.itm_ma_h) ,")")
-        print("itm_ma_l:", self.itm_ma_l, "(", type(self.itm_ma_l) ,")")
-        print("itm_ma_oi:", self.itm_ma_oi, "(", type(self.itm_ma_oi) ,")")
-        print("itm_reentry_after_mins:", self.itm_reentry_after_mins, "(", type(self.itm_reentry_after_mins) ,")")
-        print("itm_entry_points_difference:", self.itm_entry_points_difference, "(", type(self.itm_entry_points_difference) ,")")
-        print("itm_exit_points_difference:", self.itm_exit_points_difference, "(", type(self.itm_exit_points_difference) ,")")
-        print("itm_sl_points_difference:", self.itm_sl_points_difference, "(", type(self.itm_sl_points_difference) ,")")
-        print("itm_sl_cost_points_difference:", self.itm_sl_cost_points_difference, "(", type(self.itm_sl_cost_points_difference) ,")")
-        print("itm_vwap_points_difference:", self.itm_vwap_points_difference, "(", type(self.itm_vwap_points_difference) ,")")
-        print("itm_sold_option_premium_decay:", self.itm_sold_option_premium_decay, "(", type(self.itm_sold_option_premium_decay) ,")")
-        print("itm_profit_percent:", self.itm_profit_percent, "(", type(self.itm_profit_percent) ,")")
-        print("itm_profit_increment:", self.itm_profit_increment, "(", type(self.itm_profit_increment) ,")")
-        print("itm_first_target_qty:", self.itm_first_target_qty, "(", type(self.itm_first_target_qty) ,")")
-        print("itm_second_target_qty:", self.itm_second_target_qty, "(", type(self.itm_second_target_qty) ,")")
-        print("itm_order_type:", self.itm_order_type, "(", type(self.itm_order_type) ,")")
-        print("itm_last_entry_condition_check_time:", self.itm_last_entry_condition_check_time, "(", type(self.itm_last_entry_condition_check_time) ,")")
-        print("itm_pyramid_start_time:", self.itm_pyramid_start_time, "(", type(self.itm_pyramid_start_time) ,")")
-        print("itm_last_pyramid_condition_check_time:", self.itm_last_pyramid_condition_check_time, "(", type(self.itm_last_pyramid_condition_check_time) ,")")
-        print("itm_second_tranche_time_diffence_mins:", self.itm_second_tranche_time_diffence_mins, "(", type(self.itm_second_tranche_time_diffence_mins) ,")")
-        print("itm_order_qty:", self.itm_order_qty, "(", type(self.itm_order_qty) ,")")
-        print("itm_order_multiplier:", self.itm_order_multiplier, "(", type(self.itm_order_multiplier) ,")")
-        print("nifty_instrument_token:", self.nifty_instrument_token, "(", type(self.nifty_instrument_token) ,")")
-        print("nifty_hl_difference_points:", self.nifty_hl_difference_points, "(", type(self.nifty_hl_difference_points) ,")")
-        print("nifty_or_range_point_difference:", self.nifty_hl_difference_points, "(", type(self.nifty_or_range_point_difference) ,")")
-        print("nifty_or_breakout_range_point_diff:", self.nifty_or_breakout_range_point_diff, "(", type(self.nifty_or_breakout_range_point_diff) ,")")
-        print("bankNifty_instrument_token:", self.bankNifty_instrument_token, "(", type(self.bankNifty_instrument_token) ,")")
-        print("bankNifty_hl_difference_points:", self.bankNifty_hl_difference_points, "(", type(self.bankNifty_hl_difference_points) ,")")
-        print("bankNifty_or_range_point_difference:", self.bankNifty_or_range_point_difference, "(", type(self.bankNifty_or_range_point_difference) ,")")
-        print("bankNifty_or_breakout_range_point_diff:", self.bankNifty_or_breakout_range_point_diff, "(", type(self.bankNifty_or_breakout_range_point_diff) ,")")
-        print("finNifty_instrument_token:", self.finNifty_instrument_token, "(", type(self.finNifty_instrument_token) ,")")
-        print("finNifty_hl_difference_points:", self.finNifty_hl_difference_points, "(", type(self.finNifty_hl_difference_points) ,")")
-        print("finNifty_or_range_point_difference:", self.finNifty_or_range_point_difference, "(", type(self.finNifty_or_range_point_difference) ,")")
-        print("finNifty_or_breakout_range_point_diff:", self.finNifty_or_breakout_range_point_diff, "(", type(self.finNifty_or_breakout_range_point_diff) ,")")
-       
+        self.applicable_script = self.kite.applicable_scripts
+        self.instrument_token = self.kite.nifty_params[f"{self.applicable_script}_instrument_token"]
+        self.hl_difference_points = self.kite.nifty_params[f"{self.applicable_script}_hl_difference_points"]
+        self.or_range_point_difference = self.kite.nifty_params[f"{self.applicable_script}_or_range_point_difference"]
+        self.or_breakout_range_point_diff = (self.kite.nifty_params[f"{self.applicable_script}_or_breakout_range_point_diff"])       
         
 
     def historicalData(self,request):
@@ -117,7 +119,9 @@ class breakoutLogic():
 
     def itmBreakoutAlert(self, request):
 
-        past_days_required = math.ceil(self.or_breakout_candle_time*self.moving_avg_rows/375)
+        moving_rows = max(self.itm_ma_h, self.itm_ma_l)
+
+        past_days_required = math.ceil(self.or_breakout_candle_time*moving_rows/375)
         print(f'past_days_required: {past_days_required}')
 
 
@@ -126,13 +130,13 @@ class breakoutLogic():
         few_days_ago = today - datetime.timedelta(days=past_days_required+5)
 
 
-        last_few_trading_day_df = pd.DataFrame(self.kite.historicalData(request,  self.ttoken ,few_days_ago,
+        last_few_trading_day_df = pd.DataFrame(self.kite.historicalData(request, self.instrument_token, few_days_ago,
                                                                      yesterday,'minute'))
         
         last_few_trading_day_df =  resample_df(last_few_trading_day_df.copy(),
                                         x_minutes = self.or_breakout_candle_time)
         
-        last_few_trading_day_df = last_few_trading_day_df.iloc[-self.moving_avg_rows:]
+        last_few_trading_day_df = last_few_trading_day_df.iloc[-moving_rows:]
         
         # print(len(last_few_trading_day_df))
         # print(last_few_trading_day_df)
@@ -173,7 +177,7 @@ class breakoutLogic():
                 #                                                      today,'minute'))
 
 
-                curr_day_df = pd.DataFrame(self.kite.historicalData(request, self.ttoken, today, 
+                curr_day_df = pd.DataFrame(self.kite.historicalData(request, self.instrument_token, today, 
                                                                      today,'minute'))
                                 
                 curr_day_after_start_time_df = curr_day_df[curr_day_df['date'].dt.time > self.orb_range_start_time]
@@ -206,14 +210,14 @@ class breakoutLogic():
                         ###########################################################################################
                         # curr_day_df = pd.DataFrame(self.kite.historicalData(request, self.ttoken, few_days_ago, today,'minute' ))
 
-                        curr_day_df = pd.DataFrame(self.kite.historicalData(request, self.ttoken, today, today,'minute' ))
+                        curr_day_df = pd.DataFrame(self.kite.historicalData(request, self.instrument_token, today, today,'minute' ))
 
                         or_breakout_df = resample_df(curr_day_df.copy(), self.or_breakout_candle_time)
 
                         or_breakout_df = pd.concat([last_few_trading_day_df, or_breakout_df])
 
-                        or_breakout_df['ma_h'] = or_breakout_df['high'].rolling(window=self.moving_avg_rows).mean()
-                        or_breakout_df['ma_l'] = or_breakout_df['low'].rolling(window=self.moving_avg_rows).mean()
+                        or_breakout_df['ma_h'] = or_breakout_df['high'].rolling(window=self.orb_ma_h).mean()
+                        or_breakout_df['ma_l'] = or_breakout_df['low'].rolling(window=self.orb_ma_l).mean()
                         or_breakout_df['day'] = or_breakout_df['date'].dt.date
 
                         ######################################################################
@@ -227,10 +231,10 @@ class breakoutLogic():
                         if top_activator_flag:
                             return True, 'top_range_breakout'
                         
-                        bottom_activator_flag = self.test_bottom_range(request, or_breakout_df.copy(), bottom_range)
+                        # bottom_activator_flag = self.test_bottom_range(request, or_breakout_df.copy(), bottom_range)
 
-                        if bottom_activator_flag:
-                            return True, 'bottom_range_breakout'
+                        # if bottom_activator_flag:
+                        #     return True, 'bottom_range_breakout'
 
         return False
 
@@ -253,15 +257,15 @@ class breakoutLogic():
         print(c1, c2, c3, c4)
 
         ###########################
-        # if 1
+        if 1:
         
-        if c1 and c2 and c3 and c4:
+        # if c1 and c2 and c3 and c4:
         
             # print('Inside if')
             
             c5 = row['close'] - top_range < self.or_breakout_range_point_diff
             
-            if c5:
+            if c5*0:
                 print('Pre Final Breakout True ')
                 return True
             
@@ -272,11 +276,11 @@ class breakoutLogic():
 
                 while current_time() <= end_of_retracement:
                     
-                    # print(self.kite.get_ltp(request, [self.ttoken])[str(self.ttoken)]['last_price'])
-                    if (self.kite.get_ltp(request, [self.ttoken])[str(self.ttoken)]['last_price'] <= 
-                        top_range + self.or_breakout_range_point_diff):
-                        print('breakout_in_retracement')
-                        return True
+                    print(self.kite.get_ltp(request, [self.instrument_token])[str(self.instrument_token)]['last_price'])
+                    # if (self.kite.get_ltp(request, [self.instrument_token])[str(self.instrument_token)]['last_price'] <= 
+                    #     top_range + self.or_breakout_range_point_diff):
+                    #     print('breakout_in_retracement')
+                    #     return True
                 time.sleep(20)
 
         else:
@@ -322,7 +326,7 @@ class breakoutLogic():
                 while current_time() <= end_of_retracement:
                     
                     # print(self.kite.get_ltp(request, [self.ttoken])[str(self.ttoken)]['last_price'])
-                    if (self.kite.get_ltp(request, [self.ttoken])[str(self.ttoken)]['last_price'] <= 
+                    if (self.kite.get_ltp(request, [self.instrument_token])[str(self.instrument_token)]['last_price'] <= 
                         bottom_range + self.or_breakout_range_point_diff):
                         print('breakout_in_retracement')
                         return True
---
+++ apps/home/itmEntry.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+
+class itmEntry():
+
+    def __init__(self) -> None:
+        pass
+
+    
